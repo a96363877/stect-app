00bd0be94a5f84de53fa815a8e803654015c7f44
a96363877 <a96363877@gmail.com>
2025-07-02 19:19:56 +0300

    بي

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Qiuck App',
+  description: 'احصل على افضل العروض والخدمات',
 }
 
 export default function RootLayout({
